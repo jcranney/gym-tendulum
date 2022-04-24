da5fd7e94a1f7eb90e1c7643da670a1be5e9fb19
Jesse Cranney <c3162562@uon.edu.au>
2022-04-24 22:45:13 +1000

    added control dependency for example

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='gym_tendulum',
-      version='0.0.1',
-      install_requires=['gym','numpy','scipy','pygame']
+      version='0.0.2',
+      install_requires=['gym','numpy','scipy','pygame','control']
 )

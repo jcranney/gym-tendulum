68dd868babd11e1d22010c0e39b0cfa065f0770e
jcranney <c3162562@uon.edu.au>
2022-05-03 13:38:31 +1000

    formatting

--- setup.py
+++ setup.py
@@ -2,5 +2,11 @@ from setuptools import setup
 
 setup(name='gym_tendulum',
       version='0.0.1',
-      install_requires=['gym','numpy','scipy','pygame','pygame_recorder @ git+https://github.com/jcranney/pygame_recorder']
+      install_requires=[
+            'gym',
+            'numpy',
+            'scipy',
+            'pygame',
+            'pygame_recorder @ git+https://github.com/jcranney/pygame_recorder'
+      ]
 )
